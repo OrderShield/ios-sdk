73065097bb63cacbfed8b972943bc1cb26664fe0
yashnp <yashnp@weblineapps.com>
2026-02-25 18:23:33 +0530

    Changed the base url into stage to prod

--- OrderShieldSDK/Services/NetworkService.swift
+++ OrderShieldSDK/Services/NetworkService.swift
@@ -5,7 +5,8 @@ class NetworkService {
     static let shared = NetworkService()
     
   //stage
-    private let baseURL = "https://ordershield-api.projectbeta.biz/api/sdk"
+//    private let baseURL = "https://ordershield-api.projectbeta.biz/api/sdk"
+    private let baseURL = "https://api.ordershield.ai/api/sdk"
     
     //prod
 //    private let baseURL = "https://api.ordershield.ai/api/sdk"
